3344c8112d5ca75ec3ddceba0dc158bf88a3bed4
Dianne Merabueno <111546446+dnnmerabuenooo@users.noreply.github.com>
2024-02-02 23:38:28 +0800

    Update MainActivity.java

--- java/MainActivity.java
+++ java/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.labact;
+package com.example.presidentialelections;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,22 +27,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitAge(View v) {
-        if (et_age.getText().toString().isEmpty()) {
+        String enteredName = home.getText().toString();
+        String enteredAge = et_age.getText().toString();
+
+        if (enteredName.isEmpty() && enteredAge.isEmpty()) {
+            Toast.makeText(this, "Please enter your name and age.", Toast.LENGTH_SHORT).show();
+        } else if (enteredName.isEmpty()) {
+            Toast.makeText(this, "Please enter your name.", Toast.LENGTH_SHORT).show();
+        } else if (enteredAge.isEmpty()) {
             Toast.makeText(this, "Please enter your age.", Toast.LENGTH_SHORT).show();
         } else {
-            age = Integer.parseInt(et_age.getText().toString());
+            age = Integer.parseInt(enteredAge);
             if (age <= 17) {
                 Toast.makeText(this, "You cannot vote.", Toast.LENGTH_SHORT).show();
             } else {
                 Toast.makeText(this, "You can vote!", Toast.LENGTH_SHORT).show();
 
-                name = home.getText().toString();
+                name = enteredName;
                 Toast.makeText(this, "Hello, " + name + "!", Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(this, Page1.class);
+                Intent i = new Intent(this, VotingActivity.class);
                 i.putExtra("name", name);
                 startActivity(i);
                 finish();
             }
         }
     }
+
 }
